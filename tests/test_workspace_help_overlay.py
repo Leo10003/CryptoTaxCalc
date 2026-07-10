fff1bb3d2913aac0cb9fa5798cb0327adbbac231
Leo10003 <leonardo.piccinelli@gmail.com>
2026-07-10 20:23:20 +0200

    Polish client support reporting flow

--- tests/test_workspace_help_overlay.py
+++ tests/test_workspace_help_overlay.py
@@ -25,6 +25,10 @@ def test_workspace_results_help_button_has_overlay_and_fallback():
     assert 'data-help-open="true"' in html
     assert 'id="rsHelpOverlay"' in html
     assert 'id="rsHelpCreateReport"' in html
-    assert 'id="rsHelpRefreshHistory"' in html
-    assert "/support/report-issue/history?limit=5" in html
+    assert 'id="rsHelpRefreshHistory"' not in html
+    assert 'id="rsHelpHistory"' not in html
+    assert "/support/report-issue/history" not in html
+    assert "Support token" not in html
+    assert "rsHelpToken" not in html
+    assert "/support/report-issue/history?limit=5" not in html
     assert "workspace_results_help_overlay" in html
